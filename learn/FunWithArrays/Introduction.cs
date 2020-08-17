d4c86080efec045a4f043d1c883914a186adc87f
Hasanuzzaman <shuvo009@yahoo.com>
2020-08-17 23:12:04 +0600

    FindMaxConsecutiveOnes

---
+++ learn/FunWithArrays/Introduction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace leetcode.learn.FunWithArrays
+{
+    public class Introduction
+    {
+        //Given a binary array, find the maximum number of consecutive 1s in this array.
+
+        //Input: [1,1,0,1,1,1]
+        //Output: 3
+        //Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            var globalMax = 0;
+            var localMax = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 1)
+                {
+                    localMax++;
+                }
+                else
+                {
+                    globalMax = Math.Max(globalMax, localMax);
+                    localMax = 0;
+                }
+            }
+
+            return Math.Max(globalMax, localMax);
+        }
+    }
+}
---
+++ learn/FunWithArrays/introduction_test.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace leetcode.learn.FunWithArrays
+{
+    public class introduction_test
+    {
+        [Fact]
+        public void Find_Max_Consecutive_Ones_Test()
+        {
+            var introduction = new Introduction();
+
+            var maxConsecutive = introduction.FindMaxConsecutiveOnes(new int[] { 1, 1, 0, 1, 1, 1 });
+            Assert.Equal(3, maxConsecutive);
+
+            maxConsecutive = introduction.FindMaxConsecutiveOnes(new int[] { 1, 1, 1, 0, 1, 0, 0 });
+            Assert.Equal(3, maxConsecutive);
+
+            maxConsecutive = introduction.FindMaxConsecutiveOnes(new int[] { 0, 0, 0, 0, 0, 0, 0 });
+            Assert.Equal(0, maxConsecutive);
+
+            maxConsecutive = introduction.FindMaxConsecutiveOnes(new int[] { 0, 0, 0, 1, 0, 1, 0 });
+            Assert.Equal(1, maxConsecutive);
+
+            maxConsecutive = introduction.FindMaxConsecutiveOnes(new int[] { 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1 });
+            Assert.Equal(6, maxConsecutive);
+
+        }
+    }
+}
