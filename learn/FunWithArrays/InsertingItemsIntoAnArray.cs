5a08d77fc4131c042689f049b4e3536bec84bbc0
Hasanuzzaman <shuvo009@yahoo.com>
2020-08-19 22:40:27 +0600

    Duplicate Zeros

---
+++ learn/FunWithArrays/InsertingItemsIntoAnArray.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode.learn.FunWithArrays
+{
+    public class InsertingItemsIntoAnArray
+    {
+        public void DuplicateZeros(int[] arr)
+        {
+            int curentIndex = 0;
+            while (curentIndex < arr.Length)
+            {
+                if (arr[curentIndex] == 0)
+                {
+                    for (int i = arr.Length - 2; i > curentIndex; i--)
+                        arr[i + 1] = arr[i];
+
+                    if (++curentIndex <= arr.Length - 1)
+                        arr[curentIndex] = 0;
+                }
+
+                curentIndex++;
+            }
+        }
+    }
+}
---
+++ learn/FunWithArrays/InsertingItemsIntoAnArray_test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace leetcode.learn.FunWithArrays
+{
+    public class InsertingItemsIntoAnArray_test
+    {
+        [Fact]
+        public void Duplicate_Zeros_Test()
+        {
+            var insertingItemsIntoAnArray = new InsertingItemsIntoAnArray();
+
+
+            var testInputOne = new int[] { 1, 0, 2, 3, 0, 4, 5, 0 };
+            insertingItemsIntoAnArray.DuplicateZeros(testInputOne);
+            Assert.Equal(new int[] { 1, 0, 0, 2, 3, 0, 0, 4 }, testInputOne);
+            
+            
+            var testInputTwo = new int[] { 1, 2, 3 };
+            insertingItemsIntoAnArray.DuplicateZeros(testInputTwo);
+            Assert.Equal(new int[] { 1, 2, 3 }, testInputTwo);
+            
+            var testInputThree = new int[] { 0, 0, 0, 0, 0, 0, 0 };
+            insertingItemsIntoAnArray.DuplicateZeros(testInputThree);
+            Assert.Equal(new int[] { 0, 0, 0, 0, 0, 0, 0 }, testInputThree);
+
+        }
+    }
+}
