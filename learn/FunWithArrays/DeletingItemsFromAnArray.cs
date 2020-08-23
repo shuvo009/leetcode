f02f20c30a266e75151203cc5c15cdbe895e2ebb
Hasanuzzaman <shuvo009@yahoo.com>
2020-08-23 13:34:57 +0600

    Remove Element

---
+++ learn/FunWithArrays/DeletingItemsFromAnArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace leetcode.learn.FunWithArrays
+{
+    public class DeletingItemsFromAnArray
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            var length = nums.Length;
+            var arraryLength = nums.Length - 1;
+            for (int i = arraryLength; i >= 0; i--)
+            {
+                if (nums[i] == val)
+                {
+                    if (i != arraryLength)
+                    {
+                        for (int j = i; j < nums.Length-1; j++)
+                        {
+                            nums[j] = nums[j + 1];
+                        }
+                    }
+
+                    nums[nums.Length - 1] = 0;
+                    length--;
+                }
+
+            }
+
+            return length;
+        }
+    }
+}
---
+++ learn/FunWithArrays/DeletingItemsFromAnArray_test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace leetcode.learn.FunWithArrays
+{
+    public class DeletingItemsFromAnArray_test
+    {
+        [Fact]
+        public void Remove_Element_Test()
+        {
+            var deletingItemsFromAnArray = new DeletingItemsFromAnArray();
+
+            var inputOne = new int[] { 3, 2, 2, 3 };
+            var length = deletingItemsFromAnArray.RemoveElement(inputOne, 3);
+            Assert.Equal(2, length);
+            Assert.Equal(inputOne, new int[] { 2, 2, 0, 0 });
+
+            var inputTwo = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+            length = deletingItemsFromAnArray.RemoveElement(inputTwo, 2);
+            Assert.Equal(5, length);
+            Assert.Equal(inputTwo, new int[] { 0, 1, 3, 0, 4, 0, 0, 0 });
+
+            var inputThree = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+            length = deletingItemsFromAnArray.RemoveElement(inputThree, 9);
+            Assert.Equal(8, length);
+            Assert.Equal(inputThree, new int[] { 0, 1, 2, 2, 3, 0, 4, 2 });
+
+            var inputFour = new int[] { 0, 0, 0, 0 };
+            length = deletingItemsFromAnArray.RemoveElement(inputFour, 0);
+            Assert.Equal(0, length);
+
+        }
+    }
+}
