298899ee43f653689d5438672bc8c3e412ad6afc
Hasanuzzaman <shuvo009@yahoo.com>
2020-08-29 09:22:49 +0600

    Replace Elements with Greatest Element on Right Side

---
+++ learn/FunWithArrays/InPlaceOperations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode.learn.FunWithArrays
+{
+    public class InPlaceOperations
+    {
+        //https://leetcode.com/explore/learn/card/fun-with-arrays/511/in-place-operations/3259/
+        public int[] ReplaceElementsWithGreatestElementOnRightSide(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                var maxValue = arr[i + 1];
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (maxValue < arr[j])
+                        maxValue = arr[j];
+                }
+
+                arr[i] = maxValue;
+            }
+
+            arr[arr.Length - 1] = -1;
+            return arr;
+        }
+    }
+}
---
+++ learn/FunWithArrays/InPlaceOperations_test.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace leetcode.learn.FunWithArrays
+{
+    public class InPlaceOperations_test
+    {
+        [Fact]
+        public void Replace_Elements_With_Greatest_Element_On_Right_Side()
+        {
+            var itemsIntoAnArray = new InPlaceOperations();
+            
+            var testInputOne = new int[] { 17, 18, 5, 4, 6, 1 };
+            itemsIntoAnArray.ReplaceElementsWithGreatestElementOnRightSide(testInputOne);
+            Assert.Equal(new int[] { 18, 6, 6, 6, 1, -1 }, testInputOne);
+
+        }
+    }
+}
