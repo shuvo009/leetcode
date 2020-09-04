167cf426988021d57c3ee0588245887428ba75e9
Hasanuzzaman <shuvo009@yahoo.com>
2020-09-04 12:45:09 +0600

    Design Linked List

---
+++ learn/LinkedList/MyLinkedList.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace leetcode.learn.LinkedList
+{
+    public class MyLinkedList
+    {
+        private class Node
+        {
+            public Node(int value)
+            {
+                Value = value;
+            }
+
+            public int Value { get; private set; }
+            public Node NextNode { get; set; }
+        }
+
+        Node _head;
+
+        /** Initialize your data structure here. */
+        public MyLinkedList()
+        {
+
+        }
+
+        /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+        public int Get(int index)
+        {
+            if (_head == null)
+                return -1;
+            var tempIndex = 0;
+            var temNode = _head;
+            while (temNode != null)
+            {
+                if (tempIndex == index)
+                    return temNode.Value;
+                temNode = temNode.NextNode;
+                tempIndex++;
+            }
+
+            return -1;
+        }
+
+        /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+        public void AddAtHead(int val)
+        {
+            var tempNode = new Node(val);
+            tempNode.NextNode = _head;
+            _head = tempNode;
+        }
+
+        /** Append a node of value val to the last element of the linked list. */
+        public void AddAtTail(int val)
+        {
+            var lastNode = _head;
+            while (lastNode.NextNode != null)
+            {
+                lastNode = lastNode.NextNode;
+            }
+            lastNode.NextNode = new Node(val);
+        }
+
+        /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+        public void AddAtIndex(int index, int val)
+        {
+            if (index < 0)
+                return;
+
+            if (index == 0)
+            {
+                var newHead = new Node(val);
+                newHead.NextNode = _head;
+                _head = newHead;
+                return;
+            }
+
+            var currentNode = _head;
+            Node previousNode = null;
+
+            var i = 0;
+            while (currentNode != null & i < index)
+            {
+                previousNode = currentNode;
+                currentNode = currentNode.NextNode;
+                i++;
+            }
+
+            if (i == index)
+            {
+                var newNode = new Node(val);
+                newNode.NextNode = currentNode;
+                previousNode.NextNode = newNode;
+            }
+        }
+
+        /** Delete the index-th node in the linked list, if the index is valid. */
+        public void DeleteAtIndex(int index)
+        {
+            if (index < 0)
+                return;
+
+            if (index == 0)
+            {
+                _head = _head.NextNode;
+                return;
+            }
+
+            var currentNode = _head;
+            Node previousNode = null;
+
+            var i = 0;
+            while (currentNode != null & i < index)
+            {
+                previousNode = currentNode;
+                currentNode = currentNode.NextNode;
+                i++;
+            }
+
+            if (i == index)
+                previousNode.NextNode = currentNode?.NextNode;
+        }
+
+
+        private Node GetNodeByIndex(int index)
+        {
+
+
+            return null;
+        }
+    }
+
+    /**
+     * Your MyLinkedList object will be instantiated and called as such:
+     * MyLinkedList obj = new MyLinkedList();
+     * int param_1 = obj.Get(index);
+     * obj.AddAtHead(val);
+     * obj.AddAtTail(val);
+     * obj.AddAtIndex(index,val);
+     * obj.DeleteAtIndex(index);
+     */
+}
---
+++ learn/LinkedList/MyLinkedListTest.cs
@@ -0,0 +1,59 @@
+﻿using Xunit;
+
+namespace leetcode.learn.LinkedList
+{
+    public class MyLinkedListTest
+    {
+        [Fact]
+        public void My_Linked_List_Test_1()
+        {
+            var myLinkedList = new MyLinkedList();
+            
+            myLinkedList.AddAtHead(1);
+            
+            myLinkedList.AddAtTail(3);
+            
+            myLinkedList.AddAtIndex(1, 2);
+            
+            var val = myLinkedList.Get(1);
+            Assert.Equal(2, val);
+            myLinkedList.DeleteAtIndex(1);
+         
+            val = myLinkedList.Get(1);
+            Assert.Equal(3, val);
+        }
+        
+        
+        [Fact]
+        public void My_Linked_List_Test_2()
+        {
+            var myLinkedList = new MyLinkedList();
+            
+            myLinkedList.AddAtHead(1);
+            
+            myLinkedList.AddAtTail(3);
+            
+            myLinkedList.AddAtIndex(1, 2);
+            
+            var val = myLinkedList.Get(1);
+            Assert.Equal(2, val);
+            myLinkedList.DeleteAtIndex(0);
+         
+            val = myLinkedList.Get(0);
+            Assert.Equal(2, val);
+        }
+        
+        [Fact]
+        public void My_Linked_List_Test_3()
+        {
+            var myLinkedList = new MyLinkedList();
+            
+            myLinkedList.AddAtIndex(0, 10);
+            myLinkedList.AddAtIndex(0, 20);
+            myLinkedList.AddAtIndex(1, 30);
+            
+            var val = myLinkedList.Get(0);
+            Assert.Equal(20, val);
+        }
+    }
+}
