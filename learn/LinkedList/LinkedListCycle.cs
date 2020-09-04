6cd9a85bbccff21296bb2b61252847d65e270baf
Hasanuzzaman <shuvo009@yahoo.com>
2020-09-04 21:47:15 +0600

    LinkedListCycle

---
+++ learn/LinkedList/LinkedListCycle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode.learn.LinkedList
+{
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x)
+        {
+            val = x;
+            next = null;
+        }
+    }
+
+    public class LinkedListCycle
+    {
+        public bool HasCycle(ListNode head)
+        {
+            HashSet<ListNode> hashSet = new HashSet<ListNode>();
+            var current = head;
+            while (current!=null)
+            {
+                if (hashSet.Contains(current))
+                    return true;
+                hashSet.Add(current);
+                current = current.next;
+            }
+
+            return false;
+
+        }
+    }
+}
---
+++ learn/LinkedList/LinkedListCycleTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace leetcode.learn.LinkedList
+{
+    public class LinkedListCycleTest
+    {
+        [Fact]
+        public void Linked_List_Cycle_Test_1()
+        {
+            var linkedListCycle = new LinkedListCycle();
+            var head = new ListNode(3);
+            var node2 = new ListNode(2);
+            head.next = node2;
+
+            var node3 = new ListNode(0);
+            node2.next = node3;
+
+            var node4 = new ListNode(-4);
+            node3.next = node4;
+            node4.next = node2;
+
+            var output = linkedListCycle.HasCycle(head);
+            Assert.True(output);
+        }
+        
+        
+        [Fact]
+        public void Linked_List_Cycle_Test_2()
+        {
+            var linkedListCycle = new LinkedListCycle();
+            var head = new ListNode(1);
+            var node2 = new ListNode(2);
+            head.next = node2;
+            node2.next = head;
+
+            var output = linkedListCycle.HasCycle(head);
+            Assert.True(output);
+        }
+        
+        
+        [Fact]
+        public void Linked_List_Cycle_Test_3()
+        {
+            var linkedListCycle = new LinkedListCycle();
+            var head = new ListNode(1);
+            
+            var output = linkedListCycle.HasCycle(head);
+            Assert.False(output);
+        }
+
+    }
+}
