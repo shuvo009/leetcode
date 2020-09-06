e64ee0de706162adc0d5bcd31bff8e2643c5df18
Hasanuzzaman <shuvo009@yahoo.com>
2020-09-06 21:10:44 +0600

    Intersection of Two Linked Lists

---
+++ learn/LinkedList/IntersectionOfTwoLinkedLists.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode.learn.LinkedList
+{
+    public class IntersectionOfTwoLinkedLists
+    {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            HashSet<ListNode> _hashSet = new HashSet<ListNode>();
+
+            var current = headA;
+            while (current != null)
+            {
+                _hashSet.Add(current);
+                current = current.next;
+            }
+
+            current = headB;
+            while (current != null)
+            {
+                if (_hashSet.Contains(current))
+                    return current;
+                _hashSet.Add(current);
+                current = current.next;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ learn/LinkedList/IntersectionOfTwoLinkedListsTest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace leetcode.learn.LinkedList
+{
+    public class IntersectionOfTwoLinkedListsTest
+    {
+        [Fact]
+        public void Get_Intersection_Node_Test_1()
+        {
+            var intersectionOfTwoLinkedLists = new IntersectionOfTwoLinkedLists();
+            var node1 = new ListNode(4);
+            var node2 = new ListNode(1);
+            var node3 = new ListNode(8);
+            var node4 = new ListNode(4);
+            var node5 = new ListNode(5);
+            var node6 = new ListNode(5);
+            var node7 = new ListNode(6);
+            var node8 = new ListNode(1);
+
+            var listA = node1;
+            listA.next = node2;
+            node2.next = node3;
+            node3.next = node4;
+            node4.next = node5;
+
+            var listB = node6;
+            node6.next = node7;
+            node7.next = node8;
+            node8.next = node3;
+
+            var output = intersectionOfTwoLinkedLists.GetIntersectionNode(listA, listB);
+            Assert.Equal(output, node3);
+        }
+        
+        
+        [Fact]
+        public void Get_Intersection_Node_Test_2()
+        {
+            var intersectionOfTwoLinkedLists = new IntersectionOfTwoLinkedLists();
+            var node1 = new ListNode(1);
+            var node2 = new ListNode(9);
+            var node3 = new ListNode(1);
+            var node4 = new ListNode(2);
+            var node5 = new ListNode(4);
+            var node6 = new ListNode(3);
+            
+
+            var listA = node1;
+            listA.next = node2;
+            node2.next = node3;
+            node3.next = node4;
+            node4.next = node5;
+
+            var listB = node6;
+            node6.next = node4;
+
+            var output = intersectionOfTwoLinkedLists.GetIntersectionNode(listA, listB);
+            Assert.Equal(output, node4);
+        }
+        
+
+        [Fact]
+        public void Get_Intersection_Node_Test_3()
+        {
+            var intersectionOfTwoLinkedLists = new IntersectionOfTwoLinkedLists();
+            var node1 = new ListNode(2);
+            var node2 = new ListNode(6);
+            var node3 = new ListNode(4);
+
+            var node4 = new ListNode(1);
+            var node5 = new ListNode(5);
+
+            var listA = node1;
+            listA.next = node2;
+            node2.next = node3;
+            
+            var listB = node4;
+            node4.next = node5;
+
+            var output = intersectionOfTwoLinkedLists.GetIntersectionNode(listA, listB);
+            Assert.Null(output);
+        }
+    }
+}
