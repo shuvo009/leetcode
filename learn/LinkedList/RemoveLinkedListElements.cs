36e87f29ddc9b62cb6f91496914cbe3957e87aaa
Hasanuzzaman <shuvo009@yahoo.com>
2020-09-10 15:46:18 +0600

    Remove Linked List Elements

---
+++ learn/LinkedList/RemoveLinkedListElements.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode.learn.LinkedList
+{
+    public class RemoveLinkedListElements
+    {
+        //https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1207/
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            var helperNode = new ListNode(0);
+            helperNode.next = head;
+            var tempNode = helperNode;
+
+            while (tempNode.next != null)
+                if (tempNode.next.val == val)
+                    tempNode.next = tempNode.next.next;
+                else
+                    tempNode = tempNode.next;
+
+            return helperNode.next;
+        }
+    }
+}
---
+++ learn/LinkedList/RemoveLinkedListElementsTest.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+
+namespace leetcode.learn.LinkedList
+{
+    public class RemoveLinkedListElementsTest
+    {
+        public class RemoveNthNodeFromEndOfListTest
+        {
+            [Fact]
+            public void Remove_Nth_Node_From_End_Of_List_Test_1()
+            {
+                var nthNodeFromEndOfList = new RemoveLinkedListElements();
+
+                var node1 = new ListNode(1);
+                var node2 = new ListNode(2);
+                var node3 = new ListNode(6);
+                var node4 = new ListNode(3);
+                var node5 = new ListNode(4);
+                var node6 = new ListNode(5);
+                var node7 = new ListNode(6);
+
+                node1.next = node2;
+                node2.next = node3;
+                node3.next = node4;
+                node4.next = node5;
+                node5.next = node6;
+                node6.next = node7;
+
+                var nthFromEnd = nthNodeFromEndOfList.RemoveElements(node1, 6);
+                Assert.Equal("12345", Helper.StringGetValues(nthFromEnd));
+            }
+
+        }
+    }
+}
