177f2e19d34852cae809eed327a82d0db6d42096
Hasanuzzaman <shuvo009@yahoo.com>
2020-09-11 12:17:43 +0600

    Odd Even Linked List

---
+++ learn/LinkedList/OddEvenLinkedList.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+
+namespace leetcode.learn.LinkedList
+{
+    public class OddEvenLinkedList
+    {
+        //https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1208/
+        public ListNode OddEvenList(ListNode head)
+        {
+            if (head == null)
+                return head;
+            ListNode result = head;
+            ListNode tempNode1 = head;
+            ListNode tempNode2 = head.next;
+            ListNode connectedNode = head.next;
+            while (tempNode1 != null && tempNode2 != null)
+            {
+                if (tempNode2.next == null)
+                    break;
+                tempNode1.next = tempNode2.next;
+                tempNode1 = tempNode1.next;
+
+                tempNode2.next = tempNode1.next;
+                tempNode2 = tempNode2.next;
+            }
+
+            tempNode1.next = connectedNode;
+            return result;
+        }
+    }
+}
---
+++ learn/LinkedList/OddEvenLinkedListTest.cs
@@ -0,0 +1,51 @@
+﻿using Xunit;
+
+namespace leetcode.learn.LinkedList
+{
+    public class OddEvenLinkedListTest
+    {
+        [Fact]
+        public void Odd_Even_Linked_List_Test_1()
+        {
+            var oddEvenLinkedList = new OddEvenLinkedList();
+
+            var node1 = new ListNode(1);
+            var node2 = new ListNode(2);
+            var node3 = new ListNode(3);
+            var node4 = new ListNode(4);
+            var node5 = new ListNode(5);
+
+            node1.next = node2;
+            node2.next = node3;
+            node3.next = node4;
+            node4.next = node5;
+
+            var result = oddEvenLinkedList.OddEvenList(node1);
+            Assert.Equal("13524", Helper.StringGetValues(result));
+        }
+        
+        [Fact]
+        public void Odd_Even_Linked_List_Test_2()
+        {
+            var oddEvenLinkedList = new OddEvenLinkedList();
+
+            var node1 = new ListNode(2);
+            var node2 = new ListNode(1);
+            var node3 = new ListNode(3);
+            var node4 = new ListNode(5);
+            var node5 = new ListNode(6);
+            var node6 = new ListNode(4);
+            var node7 = new ListNode(7);
+
+            node1.next = node2;
+            node2.next = node3;
+            node3.next = node4;
+            node4.next = node5;
+            node5.next = node6;
+            node6.next = node7;
+
+            var result = oddEvenLinkedList.OddEvenList(node1);
+            Assert.Equal("2367154", Helper.StringGetValues(result));
+        }
+    }
+}
