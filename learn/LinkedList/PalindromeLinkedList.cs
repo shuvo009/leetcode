c21fd8029cc68311af3d17ba6241fc1a192e590b
Hasanuzzaman <shuvo009@yahoo.com>
2020-09-12 17:56:25 +0600

    Palindrome Linked List

---
+++ learn/LinkedList/PalindromeLinkedList.cs
@@ -0,0 +1,37 @@
+﻿namespace leetcode.learn.LinkedList
+{
+    public class PalindromeLinkedList
+    {
+        //https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1209/
+        public bool IsPalindrome(ListNode head)
+        {
+            if (head == null)
+                return true;
+
+            ListNode p = head;
+            ListNode prev = new ListNode(head.val);
+
+            while (p.next != null)
+            {
+                ListNode temp = new ListNode(p.next.val);
+                temp.next = prev;
+                prev = temp;
+                p = p.next;
+            }
+
+            ListNode p1 = head;
+            ListNode p2 = prev;
+
+            while (p1 != null)
+            {
+                if (p1.val != p2.val)
+                    return false;
+
+                p1 = p1.next;
+                p2 = p2.next;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ learn/LinkedList/PalindromeLinkedListTest.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+
+namespace leetcode.learn.LinkedList
+{
+    public class PalindromeLinkedListTest
+    {
+        [Fact]
+        public void Palindrome_Linked_List_Test_1()
+        {
+            var palindromeLinkedList = new PalindromeLinkedList();
+
+            var node1 = new ListNode(1);
+            var node2 = new ListNode(2);
+            
+            node1.next = node2;
+
+            var result = palindromeLinkedList.IsPalindrome(node1);
+            Assert.False(result);
+        }
+        
+        [Fact]
+        public void Palindrome_Linked_List_Test_2()
+        {
+            var palindromeLinkedList = new PalindromeLinkedList();
+
+            var node1 = new ListNode(1);
+            var node2 = new ListNode(2);
+            var node3 = new ListNode(2);
+            var node4 = new ListNode(1);
+            
+            node1.next = node2;
+            node2.next = node3;
+            node3.next = node4;
+
+            var result = palindromeLinkedList.IsPalindrome(node1);
+            Assert.True(result);
+        }
+    }
+}
