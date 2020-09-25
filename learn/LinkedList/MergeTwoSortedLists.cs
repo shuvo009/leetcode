ba064fc28738a339175d4bcd4aa355266b890d13
Hasanuzzaman <shuvo009@yahoo.com>
2020-09-25 11:45:26 +0600

    Merge Two Sorted Lists

---
+++ learn/LinkedList/MergeTwoSortedLists.cs
@@ -0,0 +1,57 @@
+﻿namespace leetcode.learn.LinkedList
+{
+    public class MergeTwoSortedLists
+    {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            if (l1 == null)
+                return l2;
+            if (l2 == null)
+                return l1;
+
+            ListNode mergedHead = null;
+
+            if (l1.val <= l2.val)
+            {
+                mergedHead = l1;
+                l1 = l1.next;
+            }
+            else
+            {
+                mergedHead = l2;
+                l2 = l2.next;
+            }
+
+            ListNode margedTail = mergedHead;
+
+            while (l1 != null && l2 != null)
+            {
+                ListNode temp = null;
+                if (l1.val <= l2.val)
+                {
+                    temp = l1;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    temp = l2;
+                    l2 = l2.next;
+                }
+
+                margedTail.next = temp;
+                margedTail = temp;
+            }
+
+            if (l1 != null)
+            {
+                margedTail.next = l1;
+            }
+            else if (l2 != null)
+            {
+                margedTail.next = l2;
+            }
+
+            return mergedHead;
+        }
+    }
+}
---
+++ learn/LinkedList/MergeTwoSortedListsTest.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+
+namespace leetcode.learn.LinkedList
+{
+    public class MergeTwoSortedListsTest
+    {
+        [Fact]
+        public void Merge_Two_Sorted_Lists_Test_1()
+        {
+            var mergeTwoSortedLists = new MergeTwoSortedLists();
+
+            var node11 = new ListNode(1);
+            var node12 = new ListNode(2);
+            var node13 = new ListNode(4);
+
+            var node21 = new ListNode(1);
+            var node22 = new ListNode(3);
+            var node23 = new ListNode(4);
+
+            node11.next = node12;
+            node12.next = node13;
+
+            node21.next = node22;
+            node22.next = node23;
+
+            var mergeTwoLists = mergeTwoSortedLists.MergeTwoLists(node11, node21);
+            Assert.Equal("112344", Helper.StringGetValues(mergeTwoLists));
+        }
+    }
+}
