13d746051c36123f599d266232e72c05127286e9
Hasanuzzaman <shuvo009@yahoo.com>
2021-06-17 23:20:11 +0600

    reverse-integer

---
+++ Problems/ReverseIntegerSolve.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace leetcode.Problems
+{
+    //https://leetcode.com/problems/reverse-integer/
+    public class ReverseIntegerSolve
+    {
+        [Theory]
+      //   [InlineData(123, 321)]
+        // [InlineData(-123, -321)]
+        // [InlineData(120, 21)]
+        // [InlineData(0, 0)]
+        // [InlineData(-10, -1)]
+        // [InlineData(10, 1)]
+        [InlineData(1534236469, 0)]
+        public void Reverse_integer_test(int input, int expected)
+        {
+            var result = Reverse(input);
+            Assert.Equal(expected, result);
+        }
+
+        public int Reverse(int x)
+        {
+            var st = x.ToString().ToCharArray();
+            if (st.Length < 1)
+                return x;
+            
+            var index = st[0] == '-' ? 1 : 0;
+            var lastIndex = st.Length - 1;
+            
+            while (index <= lastIndex)
+            {
+                var temp = st[index];
+                st[index] = st[lastIndex];
+                st[lastIndex] = temp;
+                index++;
+                lastIndex--;
+            }
+            var num = new string(st);
+            Int32.TryParse(num, out var output);
+            
+            return output;
+        }
+    }
+}
---
+++ Problems/ZigzagConversionSolve.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace leetcode.Problems
+{
+    public class ZigzagConversionSolve
+    {
+        [Theory]
+        [InlineData("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR")]
+        public void Zigzag_conversion_test(string input, int row, string expected)
+        {
+            var result = Convert(input, row);
+            Assert.Equal(expected, result);
+        }
+
+        public string Convert(string s, int numRows)
+        {
+            var length = s.Length;
+            var cycleLen = numRows * 2 - 2;
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < numRows; i++)
+            {
+                for (int j = 0; j + i < length; j += cycleLen)
+                {
+                    sb.Append(s[i + j]);
+                    if (i != 0 && i != numRows - 1 && j + cycleLen - i < length)
+                        sb.Append(s[j + cycleLen - i]);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
