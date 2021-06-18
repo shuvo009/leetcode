4b5785c47e9b9d051603cc669e988d11c95668bb
Hasanuzzaman <shuvo009@yahoo.com>
2021-06-18 23:48:03 +0600

    string-to-integer-atoi

--- Problems/ReverseIntegerSolve.cs
+++ Problems/ReverseIntegerSolve.cs
@@ -12,12 +12,12 @@ namespace leetcode.Problems
     public class ReverseIntegerSolve
     {
         [Theory]
-      //   [InlineData(123, 321)]
-        // [InlineData(-123, -321)]
-        // [InlineData(120, 21)]
-        // [InlineData(0, 0)]
-        // [InlineData(-10, -1)]
-        // [InlineData(10, 1)]
+        [InlineData(123, 321)]
+        [InlineData(-123, -321)]
+        [InlineData(120, 21)]
+        [InlineData(0, 0)]
+        [InlineData(-10, -1)]
+        [InlineData(10, 1)]
         [InlineData(1534236469, 0)]
         public void Reverse_integer_test(int input, int expected)
         {
@@ -30,10 +30,10 @@ namespace leetcode.Problems
             var st = x.ToString().ToCharArray();
             if (st.Length < 1)
                 return x;
-            
+
             var index = st[0] == '-' ? 1 : 0;
             var lastIndex = st.Length - 1;
-            
+
             while (index <= lastIndex)
             {
                 var temp = st[index];
@@ -44,7 +44,7 @@ namespace leetcode.Problems
             }
             var num = new string(st);
             Int32.TryParse(num, out var output);
-            
+
             return output;
         }
     }
---
+++ Problems/StringToIntegerSolve.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace leetcode.Problems
+{
+    //https://leetcode.com/problems/string-to-integer-atoi/
+    public class StringToIntegerSolve
+    {
+        [Theory]
+        [InlineData("42", 42)]
+        [InlineData("   -42", -42)]
+        [InlineData("4193 with words", 4193)]
+        [InlineData("words and 987", 0)]
+        [InlineData("-91283472332", -2147483648)]
+        [InlineData("91283472332", 2147483647)]
+        [InlineData("3.14159", 3)]
+        [InlineData("+-12", 0)]
+        [InlineData("-+12", 0)]
+        [InlineData("12+-", 12)]
+        [InlineData("   +0 123", 0)]
+        public void MyAtoi_test(string input, int expected)
+        {
+            var result = MyAtoi(input);
+            Assert.Equal(expected, result);
+        }
+
+
+        public int MyAtoi(string s)
+        {
+            var digits = new List<char>();
+            foreach (var a in s.TrimStart())
+            {
+                if ((a == '-' || a == '+') && digits.Count > 0)
+                    break;
+
+
+                if ((char.IsLetter(a) || char.IsPunctuation(a) || char.IsSeparator(a)) && a != '-')
+                    break;
+
+                digits.Add(a);
+            }
+
+            if (!digits.Any())
+                return 0;
+
+            var str = new string(digits.ToArray());
+
+            if (int.TryParse(str, out var x))
+                return x;
+
+            if (digits.Count < 2)
+                return 0;
+
+            return digits[0] == '-' ? int.MinValue : int.MaxValue;
+        }
+    }
+}
