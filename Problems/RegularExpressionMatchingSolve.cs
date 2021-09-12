d0a53e2b63dcc6eb89d90846f216959441a6f6be
Hasanuzzaman <shuvo009@yahoo.com>
2021-09-12 23:44:50 +0600

    RomanToIntegerSolve

---
+++ Problems/RegularExpressionMatchingSolve.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace leetcode.Problems
+{
+    //https://leetcode.com/problems/regular-expression-matching/
+    public class RegularExpressionMatchingSolve
+    {
+        [Theory]
+        [InlineData("aa", "a", false)]
+        [InlineData("aaa", "aaaa", false)]
+        [InlineData("aa", "a*", true)]
+        [InlineData("aaaa", "a*", true)]
+        [InlineData("ab", ".*", true)]
+        [InlineData("abxys", ".*", true)]
+        [InlineData("aab", "c*a*b", true)]
+        [InlineData("mississippi", "mis*is*p*.", false)]
+        [InlineData("ab", ".*c", false)]
+        public void Regular_expression_matching_solve(string input, string pattern, bool result)
+        {
+            var output = IsMatch(input, pattern);
+            Assert.Equal(output, result);
+        }
+
+
+        public bool IsMatch(string s, string p)
+        {
+            var matchLength = 0;
+            var patternIndex = 0;
+            var inputIndex = 0;
+            var currentChar = p[patternIndex];
+            var hasStar = false;
+
+
+            while (patternIndex < p.Length && matchLength < s.Length)
+            {
+                if (p[patternIndex] == '*')
+                {
+                    hasStar = true;
+                }
+                else
+                {
+                    currentChar = p[patternIndex];
+                }
+
+
+                if (s[inputIndex] == currentChar || currentChar == '.')
+                {
+                    matchLength++;
+                    inputIndex++;
+                }
+                else
+                {
+                    hasStar = false;
+                }
+
+                if (!hasStar)
+                    patternIndex++;
+            }
+
+            return matchLength == s.Length && (patternIndex >= p.Length - 1);
+        }
+    }
+}
---
+++ Problems/RomanToIntegerSolve.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace leetcode.Problems
+{
+    public class RomanToIntegerSolve
+    {
+        [Theory]
+        [InlineData("III", 3)]
+        [InlineData("IV", 4)]
+        [InlineData("IX", 9)]
+        [InlineData("LVIII", 58)]
+        [InlineData("MCMXCIV", 1994)]
+        public void RomanToIntegerTest(string input, int output)
+        {
+            var result = RomanToInt(input);
+            Assert.Equal(result, output);
+        }
+
+        public int RomanToInt(string s)
+        {
+            var single = new Dictionary<string, int>
+            {
+                {"I", 1},
+                {"V", 5},
+                {"X", 10},
+                {"L", 50},
+                {"C", 100},
+                {"D", 500},
+                {"M", 1000},
+            };
+            var doubleRoman = new Dictionary<string, int>
+            {
+                {"IV", 4},
+                {"IX", 9},
+                {"XL", 40},
+                {"XC", 90},
+                {"CD", 400},
+                {"CM", 900},
+            };
+
+            var sum = 0;
+            var charts = s.ToCharArray();
+
+            for (int i = 0; i < charts.Length; i++)
+            {
+                if (i + 1 < charts.Length)
+                {
+                    var pair = $"{charts[i]}{charts[i + 1]}";
+                    if (doubleRoman.ContainsKey(pair))
+                    {
+                        sum += doubleRoman[pair];
+                        i++;
+                        continue;
+                    }
+                }
+                sum += single[charts[i].ToString()];
+            }
+
+            return sum;
+
+        }
+    }
+}
